75616cd86e7c82ed50a2920ee9d57612ecadf781
Robert Cole <robertncole1@gmail.com>
2023-10-12 20:06:33 -0400

    working through deployment and coding

---
+++ src/routes/tasks/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import db from '../../firebase.js';
+	import { addDoc, collection } from 'firebase/firestore';
+	/**
+	 * @type {any}
+	 */
+	let input;
+	const handleSubmit = async (/** @type {{ preventDefault: () => void; }} */ e) => {
+		e.preventDefault();
+		await addDoc(collection(db, 'tasks'), {
+			todoText: input,
+			Id: crypto.randomUUID()
+		});
+	};
+</script>
+
+<div class="task-container h-screen bg-yellow-100">
+	<form on:submit={handleSubmit}>
+		<input type="text" bind:value={input} />
+		<button type="submit">Submit</button>
+	</form>
+</div>
+
+<style>
+</style>
